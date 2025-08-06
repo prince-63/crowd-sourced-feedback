c2bcabf3304d4683b49f57ac5dc20b9e235e31a9
Prince Kumar Prasad <prince08833@gmail.com>
2025-08-06 11:23:31 +0530

    feat: use user route in app

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+import createError from 'http-errors';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import userRoute from './routes/userRoute.js';
 
 const app = express();
 
@@ -10,11 +10,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(userRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-module.exports = app;
+export default app;
