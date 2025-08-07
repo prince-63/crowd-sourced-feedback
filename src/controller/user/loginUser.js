41d997adbc5417c79165710b6352780d6b02c0da
Prince Kumar Prasad <prince08833@gmail.com>
2025-08-07 16:04:45 +0530

    refactor: removed register response dto and use error mapper and user mapper

--- src/controller/user/loginUser.js
+++ src/controller/user/loginUser.js
@@ -1,31 +1,21 @@
 import UserService from "../../services/UserService.js";
 import LoginResponseDTO from "../../dto/LoginResponseDTO.js";
 import ResponseDTO from "../../dto/ResponseDTO.js";
-import ErrorResponseDTO from "../../dto/ErrorResponseDTO.js";
 import jwtHelper from "../../utils/JwtHelper.js";
 import ApplicationConstants from "../../../constants/ApplicationConstants.js";
+import ErrorMapper from "../../mapper/ErrorMapper.js";
 
 const loginUser = async (req, res) => {
     const userData = req.body;
-
     try {
         const user = await UserService.loginUser(userData);
-
         const token = jwtHelper.signIn(user.id, user.email, user.role);
-
-        const responseData = new LoginResponseDTO(
-            token
-        );
-
+        const responseData = new LoginResponseDTO(token);
         res.header(ApplicationConstants.AUTHORIZATION, token);
         res.status(200).send(new ResponseDTO("Logged Successfully.", responseData));
     }
     catch (error) {
-        const errorResponse = new ErrorResponseDTO(
-            req.path,
-            500,
-            error.message
-        )
+        const errorResponse = ErrorMapper.mapToErrorResponseDTO(error, req);
         res.status(500).send(errorResponse);
     }
 }
--- src/controller/user/registerUser.js
+++ src/controller/user/registerUser.js
@@ -1,29 +1,17 @@
 import UserService from "../../services/UserService.js";
-import RegisterResponseDTO from "../../dto/RegisterResponseDTO.js";
 import ResponseDTO from "../../dto/ResponseDTO.js";
-import ErrorResponseDTO from "../../dto/ErrorResponseDTO.js";
+import UserMapper from "../../mapper/UserMapper.js";
+import ErrorMapper from "../../mapper/ErrorMapper.js";
 
 const registerUser = async (req, res) => {
     const userData = req.body;
-
     try {
         const savedUser = await UserService.registerUser(userData);
-
-        const responseData = new RegisterResponseDTO(
-            savedUser.id,
-            savedUser.name,
-            savedUser.email,
-            savedUser.role
-        );
-
+        const responseData = UserMapper.mapToUserResponseDTO(savedUser);
         res.status(200).send(new ResponseDTO("Successfully registered", responseData));
     }
     catch (error) {
-        const errorResponse = new ErrorResponseDTO(
-            req.path,
-            500,
-            error.message
-        )
+        const errorResponse = ErrorMapper.mapToErrorResponseDTO(error, req);
         res.status(500).send(errorResponse);
     }
 }
