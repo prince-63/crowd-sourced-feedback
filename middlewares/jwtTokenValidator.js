6d09a4da9b4b71228c4aad6e2d09c7779139926e
Prince Kumar Prasad <prince08833@gmail.com>
2025-08-07 15:55:37 +0530

    feat: add jwt validation filter middleware

---
+++ middlewares/jwtTokenValidator.js
@@ -0,0 +1,30 @@
+import ApplicationConstants from "../constants/ApplicationConstants.js";
+import ErrorResponseDTO from "../src/dto/ErrorResponseDTO.js";
+import jwtHelper from "../src/utils/JwtHelper.js";
+
+const jwtTokenValidator = async (req, res, next) => {
+    const token = req.header(ApplicationConstants.AUTHORIZATION);
+
+    if (!token) {
+        res.status(401).send(
+            new ErrorResponseDTO(req.path, 401, "no token provided")
+        );
+    }
+
+    const payload = jwtHelper.verifyToken(token, process.env.JWT_SECRET);
+
+    if (payload.exp < new Date().getTime()) {
+        res.status(401).send(
+            new ErrorResponseDTO(req.path, 401, "your token is expired!")
+        )
+    }
+
+    req.body = {
+        userId: payload.userId,
+        email: payload.email,
+        role: payload.role,
+    };
+    next();
+}
+
+export default jwtTokenValidator;
