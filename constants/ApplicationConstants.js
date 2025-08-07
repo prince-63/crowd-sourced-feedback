061b3f0a95bb15eff8bdb177218f6e7bfb914dc1
Prince Kumar Prasad <prince08833@gmail.com>
2025-08-07 15:56:30 +0530

    refactor: Authentication to Authorization name change

--- constants/ApplicationConstants.js
+++ constants/ApplicationConstants.js
@@ -1,7 +1,7 @@
 class ApplicationConstants {
     static JWT_SECRET = process.env.JWT_SECRET;
     static JWT_DEFAULT_SECRET = "jxgEQeXHuPq8VdbyYFNkANdudQ53YUn4";
-    static AUTHENTICATION = "Authentication";
+    static AUTHORIZATION = "Authorization";
     static JWT_TOKEN_EXPIRATION = new Date(new Date().getTime() + 3600 * 60 * 1000);
 }
 
